6186b055361a7050afdb51ced0a625a5dde5bb12
Aleksandar Lazarevic <aleksandar.lazarevic90@gmal.com>
2021-03-10 19:01:59 +0100

    Added location search component.

--- src/app/forecast/forecast.module.ts
+++ src/app/forecast/forecast.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ForecastComponent } from './pages/forecast/forecast.component';
+import { LocationSearchComponent } from './components/location-search/location-search.component';
 
 
 
 @NgModule({
-  declarations: [ForecastComponent],
+  declarations: [ForecastComponent, LocationSearchComponent],
   imports: [
     CommonModule
   ]
