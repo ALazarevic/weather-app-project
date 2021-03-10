cc5f4158d4a70044284095ba96fd074ac7bd57fb
Aleksandar Lazarevic <aleksandar.lazarevic90@gmal.com>
2021-03-10 18:53:46 +0100

    Addd forecast page and routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForecastComponent } from './forecast/pages/forecast/forecast.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'forecast', pathMatch: 'full' },
+  { path: 'forecast', component: ForecastComponent },
+  { path: '**', redirectTo: 'forecast', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
