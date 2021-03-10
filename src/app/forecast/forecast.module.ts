c5d5372d3d25301e840917dd1703f0b897fb0d9a
Aleksandar Lazarevic <aleksandar.lazarevic90@gmal.com>
2021-03-10 19:03:34 +0100

    Added average temperature component.

--- src/app/forecast/forecast.module.ts
+++ src/app/forecast/forecast.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ForecastComponent } from './pages/forecast/forecast.component';
 import { LocationSearchComponent } from './components/location-search/location-search.component';
+import { AverageTemperatureComponent } from './components/average-temperature/average-temperature.component';
 
 
 
 @NgModule({
-  declarations: [ForecastComponent, LocationSearchComponent],
+  declarations: [ForecastComponent, LocationSearchComponent, AverageTemperatureComponent],
   imports: [
     CommonModule
   ]
